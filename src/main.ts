29e60f6800bca8dca3ba26297ccd31da6b0f7997
Ivan-rom <rom_1van@icloud.com>
2024-11-11 13:32:47 +0600

    feat: implement swagger doc via doc/api.yaml file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'node:path';
+import * as YAML from 'yamljs';
 import 'dotenv/config';
+import { SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocument = YAML.load(join(__dirname, '../doc/api.yaml'));
+
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   await app.listen(port);
 }
 bootstrap();
